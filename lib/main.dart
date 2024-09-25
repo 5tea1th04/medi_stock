15b5467b2e0fc03fd8eebbc66fc839e9d5409e57
VaidantSharma <vaidantsharma47@gmail.com>
2024-09-25 18:12:47 +0530

    Stock Analysis screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medi_stock/Screens/stockAnalysis.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: StockAnalysis(),
     );
   }
 }

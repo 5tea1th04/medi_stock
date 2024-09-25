c3ab67d2cfd9c6b15724a439bdbec82d6a6577d3
VaidantSharma <vaidantsharma47@gmail.com>
2024-09-25 16:03:19 +0530

    Screen added

---
+++ lib/Screens/stockAnalysis.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StockAnalysis extends StatefulWidget{
+  const StockAnalysis({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _StockAnalysis();
+  }
+}
+class _StockAnalysis extends State<StockAnalysis>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
